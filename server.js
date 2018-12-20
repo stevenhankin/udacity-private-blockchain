c46a6fe77b4618c2dafa4f30ee851e62b4fa3a59
Steve Hankin <steven.hankin@gmail.com>
2018-12-20 21:05:14 +0000

    refactor: Server start now an IIF
    Changed to be an Immediately Invoked Arrow Function
    to further simplify the root server script

--- server.js
+++ server.js
@@ -15,14 +15,13 @@ assignRoutes(server);
 
 
 // Start the server
-async function start() {
+(() => {
     try {
-        await server.start();
+        server.start();
     } catch (err) {
         console.log(err);
         process.exit(1);
     }
     console.log('Server running at:', server.info.uri);
-}
+})();
 
-start();
